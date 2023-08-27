82dbcaa4c812c36d62da2b7ba58e28275be50cc9
Davi Ribeiro <davi.rb@hotmail.com>
2023-08-27 11:42:45 -0300

    :sparkles: feat: Novas seções adicionadas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
+
     for (let i=0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
